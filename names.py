f351b857ecc1e1fcab5209d706443b64c5d1917b
KBrow <Brown-sw@att.net>
2016-11-04 11:40:32 -0500

    adding stars.py program

---
+++ names.py
@@ -0,0 +1,81 @@
+# =================== Instructions =====================
+# Part I
+#
+# Given the following list:
+#
+# students = [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'},
+#      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#      {'first_name' : 'KB', 'last_name' : 'Tonel'}
+# ]
+#
+# Create a program that outputs:
+#
+# Michael Jordan
+# John Rosales
+# Mark Guillen
+# KB Tonel
+#
+# Part II
+#
+# Now, given the following dictionary:
+#
+# users = {
+#  'Students': [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'},
+#      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#      {'first_name' : 'KB', 'last_name' : 'Tonel'}
+#   ],
+#  'Instructors': [
+#      {'first_name' : 'Michael', 'last_name' : 'Choi'},
+#      {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+#   ]
+#  }
+#
+# Create a program that prints  the following format (including number of characters in each combined name):
+#
+# Students
+# 1 - MICHAEL JORDAN - 13
+# 2 - JOHN ROSALES - 11
+# 3 - MARK GUILLEN - 11
+# 4 - KB TONEL - 7
+# Instructors
+# 1 - MICHAEL CHOI - 11
+# 2 - MARTIN PURYEAR - 13
+
+# =================== End of Instructions =====================
+
+
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+# ================= Code Part One ========================
+
+for student in students:
+    print (first_name, last_name)
+
+
+
+
+# ================= Code Part Two ========================
+
+
+print("Data Error, Ocra is not a valid food.")
